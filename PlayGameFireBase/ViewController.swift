91aedfde57b59c83f1dff10279109ed3f97b0cd6
NurilloUmar <nurilloumarov979@gmail.com>
2021-08-25 09:38:37 +0500

    Initial Commit

---
+++ PlayGameFireBase/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PlayGameFireBase
+//
+//  Created by apple on 25/08/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
